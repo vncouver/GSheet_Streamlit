e0f2147dc0f47c4ba57cf45b5cee4dbfd4f74bc4
vncouver <61960395+vncouver@users.noreply.github.com>
2024-06-07 15:28:56 +0300

    Update den.py

--- den.py
+++ den.py
@@ -1,1 +1,17 @@
+import streamlit as st
 
+st.title("Public Veri ÜZerinden Yayınlama")
+
+st.write("DF leri tanımladık")
+
+
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+
+url = "https://docs.google.com/spreadsheets/d/1yOdL6g16ibLUiRVa41I0QGuA6RdMKe-rcifNc-5Rh08/edit?usp=sharing"
+
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+#data = conn.read(spreadsheet=url, usecols=[0,1,4])
+data = conn.read(spreadsheet=url)
+st.dataframe(data)
